ddc3cb529c5e8d4d46be5224c96e3206e14b2ba0
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-06-09 00:06:40 +0700

    feat(data): Create note mapper to delete, insert and to domain when get

---
+++ app/src/main/java/com/nqmgaming/notesapptesting/core/data/mapper/NoteMapper.kt
@@ -0,0 +1,33 @@
+package com.nqmgaming.notesapptesting.core.data.mapper
+
+import com.nqmgaming.notesapptesting.core.data.local.NoteEntity
+import com.nqmgaming.notesapptesting.core.domain.model.NoteItem
+
+fun NoteItem.toNoteEntityForInsert(): NoteEntity {
+    return NoteEntity(
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+        dateAdded = dateAdded
+    )
+}
+
+fun NoteItem.toNoteEntityForDelete(): NoteEntity {
+    return NoteEntity(
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+        dateAdded = dateAdded,
+        id = id
+    )
+}
+
+fun NoteEntity.toNoteItem(): NoteItem {
+    return NoteItem(
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+        dateAdded = dateAdded,
+        id = id ?: 0
+    )
+}
